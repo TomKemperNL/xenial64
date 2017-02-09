9cdffc7fee81f69d0651a29f2ad0d87a53efcb55
Tom Kemper <tomkempernl@gmail.com>
2017-02-09 22:15:07 +0100

    This installs, but creates a box that wont boot...

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,3 +4,9 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y linux-headers-$(uname -r) build-essential dkms
+
+# Install vagrant key
+mkdir -pm 700 /home/vagrant/.ssh
+wget --no-check-certificate https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub -O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
